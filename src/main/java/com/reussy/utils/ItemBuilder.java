5221d23c11480b0e1f5732695e81dab152bb1413
BuildTools <unconfigured@null.spigotmc.org>
2021-05-07 23:54:24 -0600

    add method for filled background guis

--- src/main/java/com/reussy/utils/ItemBuilder.java
+++ src/main/java/com/reussy/utils/ItemBuilder.java
@@ -1,6 +1,8 @@
 package com.reussy.utils;
 
+import com.reussy.ExodusHomes;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
@@ -8,6 +10,8 @@ import org.bukkit.inventory.meta.SkullMeta;
 import java.util.List;
 
 public class ItemBuilder {
+
+	private final ExodusHomes plugin = ExodusHomes.getPlugin(ExodusHomes.class);
 
 	public ItemStack createItem(Player player, XMaterial material, int amount, String name, List<String> lore) {
 
@@ -26,6 +30,25 @@ public class ItemBuilder {
 			texture.setOwningPlayer(player);
 			item.setItemMeta(texture);
 		}
+
+		return item;
+	}
+
+	public ItemStack setBackground(Inventory inventory, XMaterial material, int amount, String name, int size, int slot) {
+
+		boolean setFill = plugin.getConfig().getBoolean("Background.Fill");
+		ItemStack item = material.parseItem();
+		item.setAmount(amount);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(name);
+		item.setItemMeta(meta);
+
+		if(setFill)
+			while(size < inventory.getSize()) {
+
+				inventory.setItem(slot, item);
+				slot++;
+			}
 
 		return item;
 	}

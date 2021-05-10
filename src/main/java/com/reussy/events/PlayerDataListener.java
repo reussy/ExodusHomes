01295bd8832307264c4be06cbb64b24837b73d44
BuildTools <unconfigured@null.spigotmc.org>
2021-05-10 11:30:03 -0600

    added listener for each player join created a new data file in storage folder

--- src/main/java/com/reussy/events/PlayerDataListener.java
+++ src/main/java/com/reussy/events/PlayerDataListener.java
@@ -1,18 +1,24 @@
 package com.reussy.events;
 
 import com.reussy.ExodusHomes;
+import com.reussy.filemanager.StorageManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.io.IOException;
 
 public class PlayerDataListener implements Listener {
 
 	private final ExodusHomes plugin = ExodusHomes.getPlugin(ExodusHomes.class);
 
 	@EventHandler
-	public void onJoin(PlayerJoinEvent e) {
+	public void onJoin(PlayerJoinEvent e) throws IOException {
 
 		Player player = e.getPlayer();
+
+		StorageManager storage = new StorageManager(player.getUniqueId());
+		storage.createPlayerFile(player);
 	}
 }
